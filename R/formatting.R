f0c1441368490e6eff6ae98b87972da165db7c7d
James J Balamuta <james.balamuta@gmail.com>
2016-02-22 12:46:38 -0600

    Updated nb2latex to include alignment for exponents
    ignored .Rhistory

--- R/formatting.R
+++ R/formatting.R
@@ -4,6 +4,7 @@
 #' scientific notation in LaTeX. E.g. 0.0003 => 3 \\cdot 10^{-4}
 #' @param x        A \code{numeric vector} to be formatted. 
 #' @param rounding A \code{integer} that indicates how many decimal places should exist.
+#' @param align    A \code{bool} that indicates whether phantom padding should be added to force column into alignment.
 #' @return
 #' A \code{character vector} containing the latex notation. 
 #' @author Stephane Guerrier & JJB
@@ -17,9 +18,27 @@
 #' nb2latex(x)
 #' 
 #' nb2latex(x, rounding = 5)
-nb2latex = function(x, rounding = 4){
-  pow = floor(log10(x))
+#' 
+#' nb2latex(x, rounding = 5, align = FALSE)
+nb2latex = function(x, rounding = 4, align = TRUE){
+  
+  pow = floor(log10(abs(x)))
   x = x/10^(pow)
   nb = round(x,rounding)
-  paste0("$",sprintf(paste0("%.",rounding,"f"),nb),"\\cdot 10^{",pow,"}$")
+  
+  n = length(x)
+  add = rep("",n)
+  
+  if(align){
+    d = nchar(pow)
+    m = max(d)
+    o = m-d
+    for(i in 1:n){
+      if(o[i] != 0){
+        add[i] = paste("\\phantom{",rep(1,o[i]),"}")
+      }
+    }
+  }
+  
+  paste0("$",sprintf(paste0("%.",rounding,"f"),nb),"\\cdot 10^{",pow,add,"}$")
 }
