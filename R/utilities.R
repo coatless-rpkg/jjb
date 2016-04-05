fa5c6e2116e3b87e58522d55bdb5d50e356c877a
coatless <balamut2@illinois.edu>
2016-04-04 23:46:18 -0500

    Recursive flag

--- R/utilities.R
+++ R/utilities.R
@@ -3,6 +3,7 @@
 #' Create a directory using either a relative path or an absolute path.
 #' 
 #' @param dir A \code{string} indicating the directory to make.
+#' @param r   A \code{boolean} that indicates whether the directories should be made recursively
 #' @return New directory on file system
 #' @examples 
 #' \dontrun{
@@ -12,14 +13,14 @@
 #' # Make directory from absolute path
 #' mkdir("C:/path/to/dir/toad")
 #' }
-mkdir = function(dir) {
+mkdir = function(dir, r = TRUE) {
   if(length(dir) != 1){
     stop("Only one directory can be created at a time.")
   }
   
   if(!dir.exists(dir)) {
     if(dir != "."){
-      dir.create(dir,  recursive = T)
+      dir.create(dir,  recursive = r)
     }
   }
 } 
