8b470c0b47b2ea5034d98e306faa8d721a8ff0a5
coatless <james.balamuta@gmail.com>
2016-04-14 23:55:00 -0500

    Added seq_bin to provide binning for integers with overflow being supplied to the last bin.

--- R/utilities.R
+++ R/utilities.R
@@ -37,3 +37,48 @@ mkdir = function(dir, r = TRUE) {
 #' is.whole(c(.4,.5,.6))
 #' is.whole(c(7,.8,9))
 is.whole = function(x){ is.numeric(x) && all(floor(x)==x) } 
+
+#' Integer Bins with Overflow
+#' 
+#' Creates a vector at equal level with the number of items per bin
+#' with the overflow going to the last bin. That is, the last bin
+#' is filled with less elements than the prior bins. 
+#' 
+#' @param n      A \code{integer} that is the total number of objects.
+#' @param splits A \code{integer} indicating the number of bins. 
+#' @param from   A \code{integer} indicating where the sequence should start.
+#' @param A \code{vector} containing integer binning. 
+#' @examples 
+#' # Evenly spaced
+#' a = seq_bin(100, 2)
+#' 
+#' b = seq_bin(100, 4)
+#' 
+#' # Oddly spaced
+#' d = seq_bin(101,5)
+#' 
+#' # First four bins equal, last differs.
+#' diff(d)
+seq_bin = function (n, bins = 2, from = 1) {
+  
+  if(!is.whole(c(n,bins,from))){ stop("`n`, `bins`, and `from` must all be whole.") }
+  
+  if(bins == 1L) return(n)
+  
+  per_tick = n/bins
+  
+  if(is.whole(per_tick)){
+    offset_val = 1L
+  }else{
+    per_tick = n/(bins-1)
+    offset_val = 2L
+  }
+  
+  o = from + (0L:(bins - offset_val)) * ceiling(per_tick)
+  
+  if(n %% bins != 0){
+    o = c(o, n)
+  }
+  
+  o
+}
