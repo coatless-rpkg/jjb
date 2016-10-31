ddea6a904439cc7f91c7f7292d4a4268176edf72
James Joseph Balamuta <balamut2@illinois.edu>
2016-10-31 12:43:26 -0500

    Remove autograder material from this package

--- R/utilities.R
+++ R/utilities.R
@@ -1,3 +1,18 @@
+# Copyright (C) 2015  James Balamuta
+#
+# This file is part of `balamuta` R Package
+#
+# The `balamuta` R package is free software: you can redistribute it and/or modify it
+# under the terms of the MIT LICENSE included within the packages source as the LICENSE file.
+#
+# The `balamuta` R package is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# You should have received a copy of the MIT License along with `balamuta`. 
+# If not, see <https://opensource.org/licenses/MIT>.
+
+
 #' Make Directory
 #' 
 #' Create a directory using either a relative path or an absolute path.
