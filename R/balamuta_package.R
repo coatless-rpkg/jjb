f6736c0c269ff6e63b666e85447eadf11fb2992f
James J Balamuta <balamut2@illinois.edu>
2017-09-10 23:42:59 -0500

    Package trick

--- R/balamuta_package.R
+++ R/balamuta_package.R
@@ -1,23 +1,4 @@
-#' Balamuta R Package
-#'
-#' The package contains functions useful functions for doing analysis within R, working with graphics, and strings.
-#' 
-#' @details 
-#' \tabular{ll}{
-#' Package: \tab balamuta\cr
-#' Type: \tab Package\cr
-#' Version: \tab 1.0.0\cr
-#' Date: \tab 2015-12-06\cr
-#' License: \tab MIT + file LICENSE\cr
-#' }
-#' 
-#' @author
-#' James Balamuta \email{balamut2@@illinois.edu}
-#'
-#' James Balamuta \email{balamut2@@illinois.edu}
-#' @name balamuta-package
 #' @importFrom grDevices graphics.off hcl
 #' @importFrom stats quantile
 #' @importFrom utils write.table
-#' @docType package
-NULL
+"_PACKAGE"
