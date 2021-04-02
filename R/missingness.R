45a2023dab78f1bb079e7d57563bf5f6266f76b0
James Balamuta <balamut2@illinois.edu>
2021-04-02 13:46:08 -0500

    Add copyright header

--- R/missingness.R
+++ R/missingness.R
@@ -1,3 +1,18 @@
+# Copyright (C) 2021  James Balamuta
+#
+# This file is part of `jjb` R Package
+#
+# The `jjb` R package is free software: you can redistribute it and/or modify
+# it under the terms of the GPL-3 LICENSE included within the packages source 
+# as the LICENSE file.
+#
+# The `jjb` R package is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# You should have received a copy of the GPL-3 License along with `jjb`.
+# If not, see <https://opensource.org/licenses/GPL-3.0>.
+
 #' Determine Amount of Missing Values in Data
 #' 
 #' Computes the proportion of data that is missing in a given data set.
