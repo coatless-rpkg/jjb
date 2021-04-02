1f06310e03ca7d5a12f8a392ce5aaa2d5c33d1d0
James Balamuta <balamut2@illinois.edu>
2021-04-02 13:45:58 -0500

    Add count vs. prop

--- R/missingness.R
+++ R/missingness.R
@@ -6,38 +6,64 @@
 #' 
 #' @return 
 #' 
-#' - Overall: a single numeric value between `[0, 1]`.
-#' - Variable: \eqn{P} different numeric values between `[0, 1]`.
-#' - Observation: \eqn{N} different numeric values between `[0, 1]`.
+#' - Overall: a single numeric value between `[0, 1]` or a count between `[0, N]`.
+#' - Variable: \eqn{P} different numeric values between `[0, 1]` or counts between `[0, N]`.
+#' - Observation: \eqn{N} different numeric values between `[0, 1]` or counts between `[0, P]`.
 #' 
 #' @examples 
 #' # By vector
 #' x = c(1, 2, NA, 4)
-#' na_overall(x)
+#' na_prop_overall(x)
+#' na_count_overall(x)
 #' 
 #' # By Data Frame
 #' missing_df = data.frame(
 #'  a = c(1, 2, NA, 4),
 #'  b = c(3, NA, 2, NA)
 #' )
-#' na_overall(missing_df)
-#' na_by_variable(missing_df)
-#' na_by_observation(missing_df)
+#' 
+#' # Proportion
+#' na_prop_overall(missing_df)
+#' na_prop_by_variable(missing_df)
+#' na_prop_by_observation(missing_df)
+#' 
+#' # Counts
+#' na_count_overall(missing_df)
+#' na_count_by_variable(missing_df)
+#' na_count_by_observation(missing_df)
 #' 
 #' @export
 #' @rdname na_summaries
-na_overall = function(x) {
+na_prop_overall = function(x) {
   mean(is.na(x))
 }
 
 #' @export
 #' @rdname na_summaries
-na_by_variable = function(x) {
+na_prop_by_variable = function(x) {
   colMeans(is.na(x))
 }
 
 #' @export
 #' @rdname na_summaries
-na_by_observation = function(x) {
+na_prop_by_observation = function(x) {
   rowMeans(is.na(x))
+}
+
+#' @export
+#' @rdname na_summaries
+na_count_overall = function(x) {
+  sum(is.na(x))
+}
+
+#' @export
+#' @rdname na_summaries
+na_count_by_variable = function(x) {
+  colSums(is.na(x))
+}
+
+#' @export
+#' @rdname na_summaries
+na_count_by_observation = function(x) {
+  rowSums(is.na(x))
 }
